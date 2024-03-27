732c11e4f6fb0b4263df76fc54bf89011b0dcc09
LakshanShalintha <lakshanshalintha2001@gmail.com>
2024-03-28 01:01:56 +0530

    edit Login

--- lib/LogInPage.dart
+++ lib/LogInPage.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'SignUpPage.dart';
 import 'MyHomePage.dart';
 
 class LogInPage extends StatefulWidget {
-  const LogInPage({super.key});
+  const LogInPage({Key? key}) : super(key: key);
 
   @override
   _LogInPageState createState() => _LogInPageState();
@@ -13,6 +12,7 @@ class LogInPage extends StatefulWidget {
 class _LogInPageState extends State<LogInPage> {
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  bool _showPassword = false; // Boolean to track the state of showing password
 
   get google => google.com;
   get facebook => facebook.com;
@@ -20,216 +20,259 @@ class _LogInPageState extends State<LogInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            PopupMenuButton(
-              itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                PopupMenuItem(
-                  child: Text('Search'),
-                  value: 'search',
-                ),
-                PopupMenuItem(
-                  child: Text('Menu'),
-                  value: 'menu',
-                ),
-                PopupMenuItem(
-                  child: Text('Log'),
-                  value: 'log',
-                ),
-                PopupMenuItem(
-                  child: Text('Sign'),
-                  value: 'sign',
-                ),
-              ],
-              onSelected: (value) {
-                // Handle item selection
-                switch (value) {
-                  case 'search':
-                  // Navigate to search page
-                    break;
-                  case 'menu':
-                  // Handle menu action
-                    break;
-                  case 'log':
-                  // Navigate to login page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SignUpPage()),
-                    );
-                    break;
-                }
-              },
-              icon: Icon(Icons.menu), // Menu icon
-            ),
-            SizedBox(width: 8), // Spacing
-            Icon(Icons.headphones), // Book icon
-            SizedBox(width: 8), // Spacing
-            Text('AudioRAB', style: TextStyle(fontSize: 20.0)),
-          ],
-        ),
-        backgroundColor: Color.fromARGB(255, 93, 147, 223),
-        actions: [
-          IconButton(
-            onPressed: () {
-              // Navigate to sign up page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => google.com()),
-              );
-            },
-            icon: Icon(Icons.search),
-          ),
-          IconButton(
-            onPressed: () {
-              // Navigate to login page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpPage()),
-              );
-            },
-            icon: Icon(Icons.person_add),
-          ),
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.red, Colors.blue], // Add your colors here
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start, // Align logo at the top
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context)
+            .unfocus(); // Dismiss the keyboard when tapping outside of text fields
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Row(
             children: [
-              Image.asset(
-                'images/logo1.jpg', // Provide your image path here
-                width: 90, // Adjust the width as needed
-                height: 90, // Adjust the height as needed
-              ),
-              Text(
-                "Real Time Audio Book",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-              SizedBox(
-                height: 250,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
+              PopupMenuButton(
+                itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                  PopupMenuItem(
+                    child: Text('Search'),
+                    value: 'search',
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(height: 5), // Spacing below logo
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextField(
-                            controller: _usernameController,
-                            decoration: InputDecoration(
-                              labelText: 'Username',
-                              prefixIcon: Icon(Icons.person),
-                            ),
-                          ),
-                          SizedBox(height: 5.0),
-                          TextField(
-                            controller: _passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              prefixIcon: Icon(Icons.password),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          SizedBox(
-                            height: 40.0, // Adjust button height as needed
-                            child: ElevatedButton(
-                              onPressed: () {
-                                // Implement sign up functionality here
-                                String username = _usernameController.text;
-                                String password = _passwordController.text;
-                                // You can add your sign up logic here
-                                print(
-                                  'Username: $username, Password: $password',
-                                );
-
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const MyHomePage(),
-                                  ),
-                                );
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                      fontSize: 16.0,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                primary:
-                                Colors.black, // Change button color here
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                  PopupMenuItem(
+                    child: Text('Menu'),
+                    value: 'menu',
                   ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // Navigate to sign up page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => google.com()),
-                      );
-                    },
-                    icon: Icon(Icons.g_mobiledata,color: Colors.white,size: 60,),
+                  PopupMenuItem(
+                    child: Text('Log'),
+                    value: 'log',
                   ),
-                  IconButton(
-                    onPressed: () {
-                      // Navigate to login page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => facebook.com()),
-                      );
-                    },
-                    icon: Icon(Icons.facebook,color: Colors.white,size: 40,),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      // Navigate to login page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => tictok.com()),
-                      );
-                    },
-                    icon: Icon(Icons.tiktok,color: Colors.white,size: 40,),
+                  PopupMenuItem(
+                    child: Text('Sign'),
+                    value: 'sign',
                   ),
                 ],
-
+                onSelected: (value) {
+                  // Handle item selection
+                  switch (value) {
+                    case 'search':
+                      // Navigate to search page
+                      break;
+                    case 'menu':
+                      // Handle menu action
+                      break;
+                    case 'log':
+                      // Navigate to login page
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpPage(),
+                        ),
+                      );
+                      break;
+                  }
+                },
+                icon: Icon(Icons.menu), // Menu icon
               ),
-
+              SizedBox(width: 8), // Spacing
+              Icon(Icons.headphones), // Book icon
+              SizedBox(width: 8), // Spacing
+              Text('AudioRAB', style: TextStyle(fontSize: 20.0)),
             ],
+          ),
+          backgroundColor: Color.fromARGB(255, 93, 147, 223),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Navigate to sign up page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => google.com()),
+                );
+              },
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {
+                // Navigate to login page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                );
+              },
+              icon: Icon(Icons.person_add),
+            ),
+          ],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.blue], // Add your colors here
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.start, // Align logo at the top
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/logo1.jpg', // Provide your image path here
+                  width: 90, // Adjust the width as needed
+                  height: 90, // Adjust the height as needed
+                ),
+                Text(
+                  "Real Time Audio Book",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+                SizedBox(
+                  height: 210,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(height: 3), // Spacing below logo
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            TextField(
+                              controller: _usernameController,
+                              decoration: InputDecoration(
+                                labelText: 'Username',
+                                labelStyle: TextStyle(
+                                    color: Colors.blue, // Change color here
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                                prefixIcon: Icon(Icons.person),
+                              ),
+                            ),
+                            SizedBox(height: 3.0),
+                            SizedBox(height: 3.0),
+                            TextField(
+                              controller: _passwordController,
+                              obscureText:
+                                  !_showPassword, // Toggle the obscure text based on the state of _showPassword
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                labelStyle: TextStyle(
+                                    color: Colors.blue, // Change color here
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                                prefixIcon: Icon(Icons.lock),
+                                suffixIcon: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _showPassword = !_showPassword;
+                                    });
+                                  },
+                                  child: Icon(
+                                    _showPassword
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        SizedBox(
+                          height: 40.0, // Adjust button height as needed
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Implement sign up functionality here
+                              String username = _usernameController.text;
+                              String password = _passwordController.text;
+                              // You can add your sign up logic here
+                              print(
+                                'Username: $username, Password: $password',
+                              );
+
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const MyHomePage(),
+                                ),
+                              );
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black, // Change button color here
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // Navigate to sign up page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => google.com()),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.g_mobiledata,
+                        color: Colors.white,
+                        size: 60,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Navigate to login page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => facebook.com()),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.facebook,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Navigate to login page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => tictok.com()),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.tiktok,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
